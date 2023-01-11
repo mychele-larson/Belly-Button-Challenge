04d26d8aa05304d7a9fb09fa8340be9f2137023e
Mychele Larson <110074895+mychele-larson@users.noreply.github.com>
2023-01-10 20:57:06 -0600

    updating homework changes

--- Belly_Button_Challenge/static/js/app2.js
+++ Belly_Button_Challenge/static/js/app2.js
@@ -1,22 +1,120 @@
-// Set up URL for data
+// // Set up URL for data
 const url =
     "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 // Promise pending for obtaining JSON data from URL
 const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
+    console.log("Data Promise: ", dataPromise);
 
-// Fetch the JSON data and console log it
+// // Fetch the JSON data and console log it
+d3.json(url).then(function(data){
+    console.log(data);
+});
+// Set up variables and get data from JSON for charts 
+var samples;
+var meta_data;
 d3.json(url).then(function (data) {
-    function createDropDown(data) {
-        var names = data.names;
-        var select = d3.select("#selDataset");
+    let selector = d3.select("#selDataset");
+    meta_data = data.metadata;
+    samples = data.samples;
+    data.names.forEach((id) => {
+        selector.append("option").text(id).property("value", id);
+    });
+    metaData(meta_data[0]);
+    hbarChart(samples[0]);
+    bubbleChart(samples[0]);
+    gaugeChart(metaData[0]);
+});
 
-        names.forEach((name) => {
-            select.append("option").text(name).property("value", name);
-        });
-    }
+function optionChanged(value) {
+    const selectedId = samples.find((item) => item.id === value);
+    const demographicInfo = meta_data.find((item) => item.id == value);
+
+    // Insterting Demographic Data
+    metaData(demographicInfo);
+
+    // Bar Chart
+    hbarChart(selectedId);
+
+    // Bubble Chart
+    bubbleChart(selectedId);
+
+    // Gauge Chart
+    gaugeChart(demographicInfo);
 }
-    // function init() {
-    //     var firstName = data.names[0];
-    //     getData(firstName)
+
+function metaData(demographicInfo) {
+    let demoSelect = d3.select("#sample-metadata");
+
+    demoSelect.html(
+        `id: ${demographicInfo.id} <br> 
+      ethnicity: ${demographicInfo.ethnicity} <br>
+    gender: ${demographicInfo.gender} <br>
+    age: ${demographicInfo.age} <br>
+    location: ${demographicInfo.location} <br>
+    bbtype: ${demographicInfo.bbtype} <br>
+    wfreq: ${demographicInfo.wfreq}`
+    );
+}
+
+function hbarChart(selectedId) {
+    let x_axis = selectedId.sample_values.slice(0, 10).reverse();
+    let y_axis = selectedId.otu_ids
+        .slice(0, 10)
+        .reverse()
+        .map((item) => `OTU ${item}`);
+    let text = selectedId.otu_labels.slice(0, 10).reverse();
+
+    barChart = {
+        x: x_axis,
+        y: y_axis,
+        text: text,
+        type: "bar",
+        orientation: "h",
+    };
+
+    let chart = [barChart];
+
+    let layout = {
+        margin: {
+            l: 100,
+            r: 100,
+            t: 0,
+            b: 100,
+        },
+        height: 500,
+        width: 600,
+    };
+
+    Plotly.newPlot("bar", chart, layout);
+}
+
+function bubbleChart(selectedId) {
+    let x_axis = selectedId.otu_ids;
+    let y_axis = selectedId.sample_values;
+    let marker_size = selectedId.sample_values;
+    let color = selectedId.otu_ids;
+    let text = selectedId.otu_labels;
+
+    bubble = {
+        x: x_axis,
+        y: y_axis,
+        text: text,
+        mode: "markers",
+        marker: {
+            color: color,
+            colorscale: "Pastel",
+            size: marker_size,
+        },
+        type: "scatter",
+    };
+    let chart = [bubble];
+
+    let layout = {
+        xaxis: {
+            title: { text: "OTU ID" },
+        },
+    };
+    Plotly.newPlot("bubble", chart, layout);
+}
+
